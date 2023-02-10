060b96f4a09e4c441a38c474de7b73635c4a3724
Gulzar Mallik <gulgermallik05@gmail.com>
2023-02-10 01:23:34 +0000

    Revert "created a shared data model for room"
    This reverts commit 33c11a5edf8bb7e961d875d74a7f384742ba33a8.

--- src/main/java/com/bsys/bms/RoomController.java
+++ src/main/java/com/bsys/bms/RoomController.java
@@ -16,8 +16,8 @@ import java.time.LocalDate;
 import java.util.Objects;
 
 public class RoomController {
-    static int selectedRoomId;
-    static String selectedMode;
+    private static int selectedRoomId;
+    private static String selectedMode;
     @FXML private TextField txtkeyword;
     @FXML private TableView<Rooms> tableView;
     @FXML private TableColumn<Rooms, Integer> sr_num;
@@ -40,16 +40,16 @@ public class RoomController {
     private void initialize() {
         if(RoomController.selectedRoomId > 0 && Objects.equals(RoomController.selectedMode, "E")) {
             // edit mode
-            roomTypeComboBox.setItems(RoomsDataModel.getInstance().getTypesOfRooms());
-            RoomsDataModel.getInstance().getRoomDetails(RoomController.selectedRoomId);
+            roomTypeComboBox.setItems(loadTypes());
+            displayRoomDetails(RoomController.selectedRoomId);
         }
         else if(Objects.equals(RoomController.selectedMode, "A")) {
             // add mode
-            roomTypeComboBox.setItems(RoomsDataModel.getInstance().getTypesOfRooms());
+            roomTypeComboBox.setItems(loadTypes());
         }
         else {
             // display mode
-            cmbtype.setItems(RoomsDataModel.getInstance().getTypesOfRooms());
+            cmbtype.setItems(loadTypes());
             sr_num.setCellValueFactory(new PropertyValueFactory<>("sr_num"));
             room_name.setCellValueFactory(new PropertyValueFactory<>("room_name"));
             room_capacity.setCellValueFactory(new PropertyValueFactory<>("room_capacity"));
@@ -57,7 +57,7 @@ public class RoomController {
             room_type.setCellValueFactory(new PropertyValueFactory<>("room_type"));
             active_bookings.setCellValueFactory(new PropertyValueFactory<>("active_bookings"));
             action.setCellValueFactory(new PropertyValueFactory<>("action"));
-            tableView.setItems(RoomsDataModel.getInstance().SelectDataFromRooms("1"));
+            tableView.setItems(loadRooms("1"));
         }
     }
     public void handleFilters(ActionEvent actionEvent) {
@@ -68,7 +68,7 @@ public class RoomController {
         param_str += (!room_type.isEmpty()) ? " AND room.type ='"+room_type+"'" : "";
         param_str += (!keyword.isEmpty()) ? " AND (room.name LIKE '%"+keyword+"%' OR room.detail LIKE '%"+keyword+"%' )" : "";
 
-        tableView.setItems(RoomsDataModel.getInstance().SelectDataFromRooms(param_str));
+        tableView.setItems(loadRooms(param_str));
     }
 
     public void handleReset(ActionEvent actionEvent) {
@@ -76,7 +76,7 @@ public class RoomController {
         cmbtype.setValue("");
         String param_str = "1";
 
-        tableView.setItems(RoomsDataModel.getInstance().SelectDataFromRooms(param_str));
+        tableView.setItems(loadRooms(param_str));
     }
 
     public void handleAddRoom(ActionEvent actionEvent) throws IOException {
@@ -121,7 +121,7 @@ public class RoomController {
         }
         else {
             // insert new entry
-            int result = databaseController.executeInsertQuery("INSERT INTO room(name, type, capacity, location, detail) VALUES ('" + roomName + "','" + roomType + "', " + roomCapacity + ",'" + roomLocation + "','" + roomDetails + "')");
+            int result = databaseController.executeInsertQuery("INSERT INTO room(name, type, capacity, location, detail) VALUES ('" + roomName + "','" + roomType + "','" + roomCapacity + "','" + roomLocation + "','" + roomDetails + "')");
 
             if (result > 0) {
                 // Alert user of successful insert
@@ -143,5 +143,78 @@ public class RoomController {
         }
 
         SceneController.changeScene(actionEvent, "room-edit.fxml");
+    }
+
+    private ObservableList<Rooms> loadRooms(String filter_param) {
+
+        ObservableList<Rooms> data = FXCollections.observableArrayList();
+        ResultSet resultSet = databaseController.executeSelectQuery("SELECT room.*, COUNT(booking.room_id) as active_booking FROM room LEFT JOIN booking ON room.id = booking.room_id WHERE "+filter_param+" GROUP BY room.id");
+        try {
+            int a = 1;
+            while (resultSet.next()) {
+                int selectedId = resultSet.getInt("id");
+                String roomName = resultSet.getString("name");
+                int roomCapacity = resultSet.getInt("capacity");
+                String roomDetail = resultSet.getString("detail");
+                String roomType = resultSet.getString("type");
+                int activeBookings = resultSet.getInt("active_booking");
+                Button action = new Button("Edit");
+                data.add(new Rooms(a, roomName, roomCapacity, roomDetail, roomType, activeBookings, action));
+                a++;
+
+                action.setOnAction(new EventHandler<ActionEvent>() {
+                    @Override
+                    public void handle(ActionEvent event) {
+                        RoomController.selectedMode = "E";
+                        RoomController.selectedRoomId = selectedId;
+                        try {
+                            SceneController.changeScene(event, "room-edit.fxml");
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                });
+
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return data;
+    }
+    private ObservableList<String> loadTypes() {
+        ObservableList<String> data = FXCollections.observableArrayList();
+        ResultSet resultSet = databaseController.executeSelectQuery("SELECT distinct(type) as type FROM room");
+        try {
+            while (resultSet.next()) {
+                String name = resultSet.getString("type");
+                data.add(name);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return data;
+    }
+
+    private void displayRoomDetails(int roomId) {
+        ResultSet resultSet = databaseController.executeSelectQuery("SELECT * FROM room WHERE id = " + roomId);
+        try {
+            if (resultSet.next()) {
+                String roomName = resultSet.getString("name");
+                int roomCapacity = resultSet.getInt("capacity");
+                String roomDetail = resultSet.getString("detail");
+                String roomType = resultSet.getString("type");
+                String roomLocation = resultSet.getString("location");
+
+                roomNameTextField.setText(roomName);
+                roomCapacityTextField.setText(String.valueOf(roomCapacity));
+                roomDetailTextArea.setText(roomDetail);
+                roomTypeComboBox.getSelectionModel().select(roomType);
+                roomLocationTextBox.setText(String.valueOf(roomLocation));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
