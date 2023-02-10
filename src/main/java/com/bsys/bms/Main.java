33c11a5edf8bb7e961d875d74a7f384742ba33a8
Gulzar Mallik <gulgermallik05@gmail.com>
2023-02-10 00:36:15 +0000

    created a shared data model for room

--- src/main/java/com/bsys/bms/Main.java
+++ src/main/java/com/bsys/bms/Main.java
@@ -20,7 +20,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) {
         int w = 1000; int h = 600; // static width and height for the window
-        int clerk_count = 2;
+        int clerk_count = 1;
 
         // Start a new thread for the manager window
         // This is used to create a new window for the manager interface
@@ -32,14 +32,15 @@ public class Main extends Application {
         managerThread.start();
 
         // Start a new thread for each clerk window
-        /*for(int i = 1; i<= clerk_count; i++) {
+        for(int i = 1; i<= clerk_count; i++) {
             int index = i;
             Thread clerkThread = new Thread(() -> {
                 Platform.runLater(() -> {
-                    SceneController.createWindow("Clerk " + index + " Window", "clerk-view.fxml", w, h); // display the manager interface
+                    //SceneController.createWindow("Clerk " + index + " Window", "clerk-view.fxml", w, h); // display the manager interface
+                    SceneController.createWindow("Manager " + index + " Window", "manager-view.fxml", w, h); // display the manager interface
                 });
             });
             clerkThread.start();
-        }*/
+        }
     }
 }
